2c57efce330a6cc29266abb100d605b058a44352
sheldon <sheldonmwangi254@gmail.com>
2023-06-15 12:53:17 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+number = 10;
+
+
+function add() {
+  return (number += 5);
+}
+
+function divide() {
+  return (number /= 3);
+}
+
+function subtract() {
+    return (number -= 3);
+  }
+
+  function multiply() {
+    return (number *= 3);
+  }
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+   return parseFloat(n);
+    
+}
+
+function increment(n) {
+    n++
+    return n;
+}
+
+function decrement(n) {
+    n--
+    return n;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
